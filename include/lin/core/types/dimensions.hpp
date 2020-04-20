ffcda03a545b9c5bb14351d4f0bcaff5fecb2213
kkrol27 <kyle.krol.27@gmail.com>
2020-04-19 21:26:00 -0500

    Rework the dimensions class to remove shrink lin object size

--- include/lin/core/types/dimensions.hpp
+++ include/lin/core/types/dimensions.hpp
@@ -12,34 +12,110 @@
 namespace lin {
 namespace internal {
 
-/** @class Dimension
- *  Keeps track of a single dimensions of a tensor object. */
-template <size_t N, size_t MN>
-class Dimension;
-
 /** @class Dimensions
- *  Keeps track of both dimensions of a tensor object. */
+ *  Keeps track of the size of a tensor object. */
+template <class D, typename = void>
+class Dimensions;
+
+/* Fixed row and fixed column count. */
 template <class D>
-class Dimensions {
+class Dimensions<D, std::enable_if_t<has_fixed_dimensions<D>::value>> {
  public:
+  typedef Dimensions<D, std::enable_if_t<has_fixed_dimensions<D>::value>> This;
   /* Defaulted/deleted constructors and assignment operators. */
   constexpr Dimensions() = default;
-  constexpr Dimensions(Dimensions<D> const &) = default;
-  constexpr Dimensions(Dimensions<D> &&) = default;
-  constexpr Dimensions<D> &operator=(Dimensions<D> const &) = default;
-  constexpr Dimensions<D> &operator=(Dimensions<D> &&) = default;
-  /* Dimensions getters and resizing function. */
-  constexpr size_t rows() const;
-  constexpr size_t cols() const;
-  constexpr void resize(size_t r, size_t c);
+  constexpr Dimensions(This const &) = default;
+  constexpr Dimensions(This &&) = default;
+  constexpr This &operator=(This const &) = default;
+  constexpr This &operator=(This &&) = default;
+  /* Getters and setters for tensor size. */
+  constexpr size_t rows() const { return D::Traits::Rows; }
+  constexpr size_t cols() const { return D::Traits::Cols; }
+  constexpr void resize(size_t r, size_t c) {
+    LIN_ASSERT(r == D::Traits::Rows /* Invalid argument passed to Dimensions<...>::resize */);
+    LIN_ASSERT(c == D::Traits::Cols /* Invalid argument passed to Dimensions<...>::resize */);
+  }
+};
 
+/* Variable row and fixed column count. */
+template <class D>
+class Dimensions<D, std::enable_if_t<conjunction<
+    negation<has_fixed_cols<D>>, has_bounded_cols<D>, has_fixed_rows<D>>::value>> {
  private:
-  Dimension<traits<D>::Rows, traits<D>::MaxRows> row_dimension;
-  Dimension<traits<D>::Cols, traits<D>::MaxCols> col_dimension;
+  size_t _cols;
+
+ public:
+  typedef Dimensions<D, std::enable_if_t<conjunction<
+      negation<has_fixed_cols<D>>, has_bounded_cols<D>, has_fixed_rows<D>>::value>> This;
+  /* Defaulted/deleted constructors and assignment operators. */
+  constexpr Dimensions() : _cols(D::Traits::MaxCols) { }
+  constexpr Dimensions(This const &) = default;
+  constexpr Dimensions(This &&) = default;
+  constexpr This &operator=(This const &) = default;
+  constexpr This &operator=(This &&) = default;
+  /* Getters and setters for tensor size. */
+  constexpr size_t rows() const { return D::Traits::Rows; }
+  constexpr size_t cols() const { return _cols; }
+  constexpr void resize(size_t r, size_t c) {
+    LIN_ASSERT(r == D::Traits::Rows /* Invalid argument passed to Dimensions<...>::resize */);
+    LIN_ASSERT(c > 0 && c <= D::Traits::MaxCols /* Invalid argument passed to Dimensions<...>::resize */);
+    _cols = c;
+  }
+};
+
+/* Variable row and fixed column count. */
+template <class D>
+class Dimensions<D, std::enable_if_t<conjunction<
+    has_fixed_cols<D>, negation<has_fixed_rows<D>>, has_bounded_rows<D>>::value>> {
+ private:
+  size_t _rows;
+
+ public:
+  typedef Dimensions<D, std::enable_if_t<conjunction<
+      has_fixed_cols<D>, negation<has_fixed_rows<D>>, has_bounded_rows<D>>::value>> This;
+  /* Defaulted/deleted constructors and assignment operators. */
+  constexpr Dimensions() : _rows(D::Traits::MaxRows) { }
+  constexpr Dimensions(This const &) = default;
+  constexpr Dimensions(This &&) = default;
+  constexpr This &operator=(This const &) = default;
+  constexpr This &operator=(This &&) = default;
+  /* Getters and setters for tensor size. */
+  constexpr size_t rows() const { return _rows; }
+  constexpr size_t cols() const { return D::Traits::Cols; }
+  constexpr void resize(size_t r, size_t c) {
+    LIN_ASSERT(r > 0 && r <= D::Traits::MaxRows /* Invalid argument passed to Dimensions<...>::resize */);
+    LIN_ASSERT(c == D::Traits::Cols /* Invalid argument passed to Dimensions<...>::resize */);
+    _rows = r;
+  }
+};
+
+/* Variable rows and variable column count. */
+template <class D>
+class Dimensions<D, std::enable_if_t<conjunction<
+    negation<has_fixed_cols<D>>, has_bounded_cols<D>, negation<has_fixed_rows<D>>, has_bounded_rows<D>>::value>> {
+ private:
+  size_t _rows, _cols;
+
+ public:
+  typedef Dimensions<D, std::enable_if_t<conjunction<
+      negation<has_fixed_cols<D>>, has_bounded_cols<D>, negation<has_fixed_rows<D>>, has_bounded_rows<D>>::value>> This;
+  /* Defaulted/deleted constructors and assignment operators. */
+  constexpr Dimensions() : _rows(D::Traits::MaxRows), _cols(D::Traits::MaxCols) { }
+  constexpr Dimensions(This const &) = default;
+  constexpr Dimensions(This &&) = default;
+  constexpr This &operator=(This const &) = default;
+  constexpr This &operator=(This &&) = default;
+  /* Getters and setters for tensor size. */
+  constexpr size_t rows() const { return _rows; }
+  constexpr size_t cols() const { return _cols; }
+  constexpr void resize(size_t r, size_t c) {
+    LIN_ASSERT(r > 0 && r <= D::Traits::MaxRows /* Invalid argument passed to Dimensions<...>::resize */);
+    LIN_ASSERT(c > 0 && c <= D::Traits::MaxCols /* Invalid argument passed to Dimensions<...>::resize */);
+    _rows = r;
+    _cols = c;
+  }
 };
 }  // namespace internal
 }  // namespace lin
-
-#include "inl/dimensions.inl"
 
 #endif
